905b503c92f4f1af01dd09f75e707b43c51032c5
Taotao <taotao@everants.com>
2016-11-18 12:05:33 +0800

    新增routematrix

--- lib/baidu_map.rb
+++ lib/baidu_map.rb
@@ -25,11 +25,16 @@ module BaiduMap
                      method_str.sub('_', "/v1/") + '?'
                    when 'geocoder'                   # Geocoding API
                      method_str + '/v2/?'
-                   else                              #  /^place_.*/  --- Place API and Place Suggestion API
+                   when 'routematrix_driving' #批量行驶距离计算API(驾车)
+                    '/routematrix/v2/driving?'
+                  when 'routematrix_riding'#批量行驶距离计算API(骑车)
+                    '/routematrix/v2/riding?'
+                  when 'routematrix_walking'
+                    '/routematrix/v2/walking?'#批量行驶距离计算API(步行)
+                  else                              #  /^place_.*/  --- Place API and Place Suggestion API
                      method_str.sub('_', "/v2/") + '?'
                    end
-
-      full_url = endpoint + method_url + URI.encode_www_form(params.merge(ak: @ak, output: 'json'))
+            full_url = endpoint + method_url + URI.encode_www_form(params.merge(ak: @ak, output: 'json'))
 
       block_given? ? yield(full_url) : JSON.parse(eat full_url)
     end
